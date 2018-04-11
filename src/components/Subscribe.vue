6ae367f460e38d0be5fb9b2aea1550916d01656b
evvvritt <evv.info@gmail.com>
2018-04-11 12:25:41 +0200

    Subscribe - custom description

--- src/components/Subscribe.vue
+++ src/components/Subscribe.vue
@@ -1,7 +1,7 @@
 <template>
   <form id="mc-embedded-subscribe-form" action="http://chen-williams.us7.list-manage.com/subscribe/post?u=c5bdca666893be0f93b28b913&amp;id=61fd4a23af" method="post" name="mc-embedded-subscribe-form" target="_blank" validate>
-    <label for="mce-EMAIL" class="block">Add your email to get updates and 10% off on your next purchase.</label>
-    <div class="flex items-center mt075">
+    <label for="mce-EMAIL" class="hidden">Email</label>
+    <div class="flex items-center">
       <input class="inline-block" id="mce-EMAIL" name="EMAIL" required="" type="email" value="" placeholder="email">
       <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
       <input style="position: fixed; left: -5000px; top: -5000px;" name="b_c5bdca666893be0f93b28b913_61fd4a23af" type="text" value="">
